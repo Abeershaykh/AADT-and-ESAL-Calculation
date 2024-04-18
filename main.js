cb5df29d73667077d7b55be03fbf09407d44c6d4
Abeer Shaikh <shaikhabeer4@gmail.com>
2024-04-18 21:25:52 +0500

    ESAL Calculation

---
+++ main.js
@@ -0,0 +1,27 @@
+let a = 0.9978;
+let b = 1.044;
+let c = (1 + (0.02 * 8)) * (a * b);
+let vehicle = ["Car", "Bus", "Truck", "Tanker", "Trailer"];
+let count = [100, 129, 2604, 899, 458];
+for (let i = 0; i < vehicle.length; i++) {
+    let calculation = count[i] * c;
+    let formcalculation = calculation.toFixed();
+    console.log(`${vehicle[i]} 24hr AADT is ${formcalculation}`);
+}
+// Calculating Growth Factor
+console.log("The Growth Factor is as follows");
+let growthrate = [0.065, 0.0416, 0.0416, 0.045, 0.0675, 0];
+let year = 20;
+for (let i = 0; i < growthrate.length; i++) {
+    // let growthfactor = ((1+growthrate[i]**year) -1)/growthrate[i]
+    let growthfactor = (((1 + growthrate[i]) ** year) - 1) / (growthrate[i]);
+    growthfactor = growthfactor.toFixed(2);
+    console.log(`${vehicle[i]} Growth Factor is ${growthfactor}`);
+}
+// Load Calculations
+let axle2 = [
+    { front: 0.31 },
+    { back: 0.69 }
+];
+let tonnestokilopounds = 2.2205;
+export {};
